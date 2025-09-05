5741bf79569f5606630bd650bb8d3f0b1c506053
Pranav Shinde <panushinde05@gmail.com>
2025-09-05 21:29:00 +0530

    authentication modified

--- frontend/src/components/contexts/authContext.jsx
+++ frontend/src/components/contexts/authContext.jsx
@@ -1,11 +1,13 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const [currUser, setCurrUser] = useState(() => {
     return localStorage.getItem("userId") || null;
   });
@@ -15,32 +17,46 @@ export const AuthProvider = ({ children }) => {
       const token = localStorage.getItem('token');
       const userId = localStorage.getItem('userId');
 
-      if (!token || !userId) return;
+      // public route check
+      const isPublicRoute = location.pathname.startsWith("/session");
+
+      if (!token || !userId) {
+        setCurrUser(null);
+
+        if (!isPublicRoute) {
+          navigate('/login');
+        }
+        return;
+      }
 
       try {
         const decoded = jwtDecode(token);
         const currentTime = Date.now() / 1000;
 
         if (decoded.exp < currentTime) {
-          localStorage.removeItem('token');
-          localStorage.removeItem('userId');
-          localStorage.removeItem("role")
+          console.warn("Token expired");
+          localStorage.clear();
           setCurrUser(null);
-          navigate('/login');
+
+          if (!isPublicRoute) {
+            navigate('/login');
+          }
         } else {
           setCurrUser(userId);
         }
       } catch (err) {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        localStorage.removeItem("role")
+        console.error("Invalid token:", err.message);
+        localStorage.clear();
         setCurrUser(null);
-        navigate('/login');
+
+        if (!isPublicRoute) {
+          navigate('/login');
+        }
       }
     };
 
     checkAuth();
-  }, [navigate]);
+  }, [navigate, location]);
 
   return (
     <AuthContext.Provider value={{ currUser, setCurrUser }}>
