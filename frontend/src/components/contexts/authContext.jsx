12be905bd15b3387617258173e2ac4c5590ef5a5
Pranav Shinde <panushinde05@gmail.com>
2025-09-06 15:12:59 +0530

    jwt decode modified

--- frontend/src/components/contexts/authContext.jsx
+++ frontend/src/components/contexts/authContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
